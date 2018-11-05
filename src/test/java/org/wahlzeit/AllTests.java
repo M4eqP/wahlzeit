4c578e97dbf62dd54d1561257334ac53545683c3
M4eqp <m4eqp@users.noreply.github.com>
2018-11-05 04:25:09 +0100

    Add AllTests test suite

---
+++ src/test/java/org/wahlzeit/AllTests.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2018 by M4eqP@users.noreply.github.com
+ * This file is part of the Wahlzeit photo rating application.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+package org.wahlzeit;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.wahlzeit.handlers.*;
+import org.wahlzeit.model.*;
+import org.wahlzeit.model.persistence.*;
+import org.wahlzeit.services.*;
+import org.wahlzeit.services.mailing.*;
+import org.wahlzeit.utils.*;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    // handlers tests
+    TellFriendTest.class,
+
+    // model tests
+    DatastoreAdapterTest.class,
+    AccessRightsTest.class,
+    CoordinateTest.class,
+    FlagReasonTest.class,
+    GenderTest.class,
+    GuestTest.class,
+    LocationTest.class,
+    PhotoFilterTest.class,
+    TagsTest.class,
+    UserStatusTest.class,
+    ValueTest.class,
+
+    // utils tests
+    StringUtilTest.class,
+    VersionTest.class,
+
+    // services tests
+    // new test suite
+    EmailServiceTestSuite.class,
+    // existing test case
+    EmailAddressTest.class,
+})
+public class AllTests {}
