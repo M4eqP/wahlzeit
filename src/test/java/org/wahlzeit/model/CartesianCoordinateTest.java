1252c02c7425784c073e2f7c571784a7ea1ede2c
M4eqp <m4eqp@users.noreply.github.com>
2018-12-17 04:14:29 +0100

    Make sure to use factory methods instead of constructors

--- src/test/java/org/wahlzeit/model/CartesianCoordinateTest.java
+++ src/test/java/org/wahlzeit/model/CartesianCoordinateTest.java
@@ -16,15 +16,15 @@ public class CartesianCoordinateTest {
         assertTrue(c1a.equals(c1b));
         assertTrue(c1b.equals(c1a));
         
-        CartesianCoordinate c2 = new CartesianCoordinate(1, 1, 1);
+        CartesianCoordinate c2 = Coordinate.getCartesianCoordinate(1, 1, 1);
         assertFalse(c1a.equals(c2));
         assertFalse(c2.equals(c1b));
     }
     
     @Test
     public void testIsEqual() {
-        CartesianCoordinate c1a = new CartesianCoordinate(0, 0, 0);
-        CartesianCoordinate c1b = new CartesianCoordinate(0, 0, 0);
+        CartesianCoordinate c1a = Coordinate.getCartesianCoordinate(0, 0, 0);
+        CartesianCoordinate c1b = Coordinate.getCartesianCoordinate(0, 0, 0);
 
         assertTrue(c1a.isEqual(c1a));
         assertTrue(c1b.isEqual(c1b));
@@ -32,16 +32,16 @@ public class CartesianCoordinateTest {
         assertTrue(c1a.isEqual(c1b));
         assertTrue(c1b.isEqual(c1a));
 
-        CartesianCoordinate c2 = new CartesianCoordinate(1, 1, 1);
+        CartesianCoordinate c2 = Coordinate.getCartesianCoordinate(1, 1, 1);
         assertFalse(c1a.isEqual(c2));
         assertFalse(c2.isEqual(c1b));
     }
 
     @Test
     public void testGetCartesianDistance() {
-        CartesianCoordinate c1 = new CartesianCoordinate(0, 0, 0);
-        CartesianCoordinate c2 = new CartesianCoordinate(1, 1, 1);
-        CartesianCoordinate c3 = new CartesianCoordinate(1, 2, 3);
+        CartesianCoordinate c1 = Coordinate.getCartesianCoordinate(0, 0, 0);
+        CartesianCoordinate c2 = Coordinate.getCartesianCoordinate(1, 1, 1);
+        CartesianCoordinate c3 = Coordinate.getCartesianCoordinate(1, 2, 3);
 
         assertEquals(0.0, c1.getCartesianDistance(c1), 0.000001);
         assertEquals(Math.sqrt(3), c1.getCartesianDistance(c2), 0.000001);
@@ -52,7 +52,7 @@ public class CartesianCoordinateTest {
 
     @Test
     public void testGetters() {
-        CartesianCoordinate c1 = new CartesianCoordinate(10, 20, 30);
+        CartesianCoordinate c1 = Coordinate.getCartesianCoordinate(10, 20, 30);
         assertEquals(10, c1.getX(), 0.000001);
         assertEquals(20, c1.getY(), 0.000001);
         assertEquals(30, c1.getZ(), 0.000001);
@@ -60,7 +60,7 @@ public class CartesianCoordinateTest {
 
     @Test
     public void testConversionFromAndToSphericCoordinate() {
-        CartesianCoordinate c1 = new CartesianCoordinate(10, 20 ,30);
+        CartesianCoordinate c1 = Coordinate.getCartesianCoordinate(10, 20 ,30);
         CartesianCoordinate c2 = c1.asSphericCoordinate().asCartesianCoordinate();
 
         assertEquals(c1.getX(), c2.getX(), 0.000001);
--- src/test/java/org/wahlzeit/model/SphericCoordinateTest.java
+++ src/test/java/org/wahlzeit/model/SphericCoordinateTest.java
@@ -7,8 +7,8 @@ import static org.junit.Assert.*;
 public class SphericCoordinateTest {
     @Test
     public void testEquals() {
-        SphericCoordinate c1a = new SphericCoordinate(0, 0, 0);
-        SphericCoordinate c1b = new SphericCoordinate(0, 0, 0);
+        SphericCoordinate c1a = Coordinate.getSphericCoordinate(0, 0, 0);
+        SphericCoordinate c1b = Coordinate.getSphericCoordinate(0, 0, 0);
 
         assertTrue(c1a.equals(c1a));
         assertTrue(c1b.equals(c1b));
@@ -16,15 +16,15 @@ public class SphericCoordinateTest {
         assertTrue(c1a.equals(c1b));
         assertTrue(c1b.equals(c1a));
         
-        SphericCoordinate c2 = new SphericCoordinate(1, 1, 1);
+        SphericCoordinate c2 = Coordinate.getSphericCoordinate(1, 1, 1);
         assertFalse(c1a.equals(c2));
         assertFalse(c2.equals(c1b));
     }
     
     @Test
     public void testIsEqual() {
-        SphericCoordinate c1a = new SphericCoordinate(0, 0, 0);
-        SphericCoordinate c1b = new SphericCoordinate(0, 0, 0);
+        SphericCoordinate c1a = Coordinate.getSphericCoordinate(0, 0, 0);
+        SphericCoordinate c1b = Coordinate.getSphericCoordinate(0, 0, 0);
 
         assertTrue(c1a.isEqual(c1a));
         assertTrue(c1b.isEqual(c1b));
@@ -32,16 +32,16 @@ public class SphericCoordinateTest {
         assertTrue(c1a.isEqual(c1b));
         assertTrue(c1b.isEqual(c1a));
 
-        SphericCoordinate c2 = new SphericCoordinate(1, 1, 1);
+        SphericCoordinate c2 = Coordinate.getSphericCoordinate(1, 1, 1);
         assertFalse(c1a.isEqual(c2));
         assertFalse(c2.isEqual(c1b));
     }
 
     @Test
     public void testGetSphericDistance() {
-        SphericCoordinate c1 = new SphericCoordinate(0, 0, 0);
-        SphericCoordinate c2 = new SphericCoordinate(1, 1, 1);
-        SphericCoordinate c3 = new SphericCoordinate(1, 2, 3);
+        SphericCoordinate c1 = Coordinate.getSphericCoordinate(0, 0, 0);
+        SphericCoordinate c2 = Coordinate.getSphericCoordinate(1, 1, 1);
+        SphericCoordinate c3 = Coordinate.getSphericCoordinate(1, 2, 3);
 
         assertEquals(0.0, c1.getCentralAngle(c1), 0.000001);
 
@@ -53,7 +53,7 @@ public class SphericCoordinateTest {
 
     @Test
     public void testGetters() {
-        SphericCoordinate c1 = new SphericCoordinate(2, 3, 30);
+        SphericCoordinate c1 = Coordinate.getSphericCoordinate(2, 3, 30);
         assertEquals(2, c1.getPhi(), 0.000001);
         assertEquals(3, c1.getTheta(), 0.000001);
         assertEquals(30, c1.getRadius(), 0.000001);
@@ -61,7 +61,7 @@ public class SphericCoordinateTest {
 
     @Test
     public void testConversionFromAndToCartesianCoordinate() {
-        SphericCoordinate c1 = new SphericCoordinate(-3, 2 ,30);
+        SphericCoordinate c1 = Coordinate.getSphericCoordinate(-3, 2 ,30);
         SphericCoordinate c2 = c1.asCartesianCoordinate().asSphericCoordinate();
 
         assertEquals(c1.getPhi(), c2.getPhi(), 0.001);
@@ -71,7 +71,7 @@ public class SphericCoordinateTest {
 
     private void assertConstructorThrowsInvalidArgumentException(double phi, double theta, double radius) {
         try {
-            new SphericCoordinate(phi, theta, radius);
+            Coordinate.getSphericCoordinate(phi, theta, radius);
         } catch (IllegalArgumentException e) {
             return;
         }
@@ -83,9 +83,9 @@ public class SphericCoordinateTest {
     @Test
     public void testConstructFromValuesOutsideRange() {
         // check borders of ranges
-        new SphericCoordinate(-Math.PI, 0, 30);
-        new SphericCoordinate(Math.PI - 0.0001, Math.PI, 30);
-        new SphericCoordinate(Math.PI - 0.0001, Math.PI, 0);
+        Coordinate.getSphericCoordinate(-Math.PI, 0, 30);
+        Coordinate.getSphericCoordinate(Math.PI - 0.0001, Math.PI, 30);
+        Coordinate.getSphericCoordinate(Math.PI - 0.0001, Math.PI, 0);
 
         // invalid phi values
         assertConstructorThrowsInvalidArgumentException(-Math.PI - 0.0001, 0, 30);
